c2a85c967f1223e3a4246bcd19d049d15981b7c4
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2012-09-19 14:13:02 +0200

    nfs: FileSystem should be able to get inode by ID and provide inode ID

--- src/org/dcache/chimera/nfs/vfs/PseudoFs.java
+++ src/org/dcache/chimera/nfs/vfs/PseudoFs.java
@@ -76,8 +76,13 @@ public class PseudoFs implements VirtualFileSystem {
     }
 
     @Override
-    public Inode inodeOf(byte[] fh) throws IOException {
-        return _inner.inodeOf(fh);
+    public Inode getInodeById(byte[] fh) throws IOException {
+        return _inner.getInodeById(fh);
+    }
+
+    @Override
+    public byte[] getInodeId(Inode inode) throws IOException {
+        return _inner.getInodeId(inode);
     }
 
     @Override
