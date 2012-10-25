d32a0d361e285fdf5028da31b71f349cd36005c6
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2012-10-25 16:53:49 +0200

    tests: fix typo in test name

--- test/org/dcache/chimera/nfs/HostEntryComparatorTest.java
+++ test/org/dcache/chimera/nfs/HostEntryComparatorTest.java
@@ -26,33 +26,38 @@ import static org.dcache.chimera.nfs.HostEntryComparator.*;
 public class HostEntryComparatorTest {
 
     @Test
-    public void souldReturnZeroIfBoughWitoutMask() {
+    public void shouldReturnZeroIfBoughWitoutMask() {
         assertEquals(0, compare("a.b.c.d", "d.e.f.g"));
     }
 
     @Test
-    public void souldReturnZeroIfBoughWitoutSameMask() {
+    public void shouldReturnZeroIfBoughWitoutSameMask() {
         assertEquals(0, compare("a.b.c.d/24", "d.e.f.g/24"));
     }
 
     @Test
-    public void souldBiggerMaskWin() {
+    public void shouldBiggerMaskWin() {
         assertEquals(1, compare("a.b.c.d/32", "d.e.f.g/24"));
     }
 
     @Test
-    public void souldWithoutMaskSWin() {
+    public void shouldWithoutMaskSWin() {
         assertEquals(1, compare("a.b.c.d", "d.e.f.g/24"));
     }
 
     @Test
-    public void souldFirstWildcatWin() {
+    public void shouldFirstWildcatWin() {
         assertEquals(1, compare("a*.somewhere.in.net", "ab*.somewhere.in.net"));
         assertEquals(-1, compare("ab*.somewhere.in.net", "a*.somewhere.in.net"));
     }
 
     @Test
-    public void souldIpBeFirst() {
+    public void shouldIpBeFirst() {
         assertEquals(1, compare("a*.somewhere.in.net", "a.b.c.d"));
+    }
+
+    @Test
+    public void shouldFullNameBeFirst() {
+        assertEquals(-1, compare("a.somewhere.in.net", "*.somewhere.in.net"));
     }
 }
